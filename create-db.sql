0b24da0d8351a628dd2650781608c7c0965d4869
Lance Armstrong <dai18026@uom.edu.gr>
2022-04-27 19:05:12 +0300

    fix

--- create-db.sql
+++ create-db.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS documents CASCADE;
 
 CREATE TABLE documents (
+<<<<<<< HEAD
     id SMALLINT PRIMARY KEY,
     body TEXT NOT NULL,
     filepath VARCHAR ( 30 ) NOT NULL,
@@ -21,3 +22,20 @@ UPDATE documents SET docvec = to_tsvector('greek', body);
 
 /* Drop original column */
 ALTER TABLE documents DROP COLUMN IF EXISTS body CASCADE;
+=======
+    id PRIMARY KEY,
+    body VARCHAR NOT NULL,
+    path VARCHAR ( 30 ) NOT NULL,
+    length SMALLINT NOT NULL,
+    size_kb INT NOT NULL,
+    time_crawled TIMESTAMP WITHOUT TIME ZONE 
+);
+
+// With Header
+COPY documents FROM PROGRAM 'awk FNR-1 /home/panos/Documents/ATD/docengine.csv | cat' DELIMITER ',' CSV;
+
+// Add Ts Vector column
+ALTER TABLE documents ADD COLUMN docvec TSVECTOR NOT NULL;
+
+UPDATE documents SET docvec = (SELECT to_tsvector(body) FROM documents);
+>>>>>>> e7133e3 (initial commit)
